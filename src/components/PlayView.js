329734a744791abe60407b0d29263fa700300a21
onurio <omrinuri@gmail.com>
2020-02-12 22:48:40 -0500

    finished faders

--- src/components/PlayView.js
+++ src/components/PlayView.js
@@ -1,115 +1,152 @@
-import React,{useState,useEffect} from 'react';
-import Tone from 'tone';
+import React,{useReducer,useState,useEffect} from 'react';
+// import Tone from 'tone';
 import Two from 'two.js';
+import {radToDeg,degToRad} from '../utils/utils';
 
 
-const degToRad=(deg)=>{
-  let pi = Math.PI;
-  return deg * pi / 180;
+
+
+let wheels;
+let two;
+
+let wheelRange = {
+    one: {
+        min: 60,
+        max: 180
+    },
+    two: {
+        min: 180,
+        max: 300
+    },
+    three: {
+        min: 120,
+        max: 240
+    }
 }
 
-let wheel;
 
-export const PlayView=props=>{
-    // const [wheel,setWheel] = useState(undefined);
+function onStart ( e ) {
+    if( navigator.userAgent.match(/Android/i && e.target.id === 'two-1')) {
+        e.preventDefault();
+    }
+}
 
-    const handleMove=(two,e)=>{
-        let rect = e.touches[0].target.parentElement.getBoundingClientRect();        
-        let touch = {x:e.touches[0].clientX,y:e.touches[0].clientY}
-        // let buttonX = rect.x;
-        // let buttonY = rect.y;
+
+export const PlayView=(props)=>{
+
+
+    const [rot,setRot] = useState(0);
+
+    const handleMove=(e,wheelNum,angleComp,range)=>{    
+        e.preventDefault();    
+        let rect = wheels[wheelNum].wheel._renderer.elem.getBoundingClientRect();    
+        let touch = {x:e.changedTouches[0].clientX,y:e.changedTouches[0].clientY}
         let x = rect.width/2 + rect.x;
         let y = rect.width/2 + rect.y;
         let button = {x:x,y:y};
         
         let result =  Math.atan2((button.y+ 20) - button.y, button.x - button.x) -  Math.atan2(touch.y - button.y, touch.x - button.x);
-        wheel.wheel.rotation = 135 - result ;
-        two.update();
-        
+
+        let resultDeg = radToDeg(result);
+        if(resultDeg < 0 ){
+            resultDeg =  270 + resultDeg + 90;
+        }        
+        if(resultDeg<range.max && resultDeg>range.min){
+            wheels[wheelNum].wheel.rotation = degToRad(angleComp) - result;
+            let effect = Math.abs(1-(resultDeg - range.min)/120);
+            wheels[wheelNum].fader.ending = effect;
+            setRot(effect);
+
+        }
         
     }
 
 
+    
+    
+
+    useEffect(()=>{        
+        if(two){
+            two.update();
+            
+        }
+    },[rot])
+
     const updateSize=()=>{
         let elem = document.getElementById('canvas');
         let params = { fullscreen: true};
-        let two = new Two(params).appendTo(elem);
+        let twoNew = new Two(params).appendTo(elem);
+        two = twoNew;
+
         let ratio = window.devicePixelRatio;
         let width = two.width;
         let height = two.height;
         let bg = two.makeRectangle(0,0,width*ratio,height*ratio);
         bg.fill = 'white';
-        let wheel1 = makeHexWheel(two,0,0,width/10);
-        let wheel2 = makeHexWheel(two,0,0,width/10);
-        let wheel3 = makeHexWheel(two,0,0,width/10);
-        wheel2.wheel.translation.set(width*0.75,height*0.875);
-        wheel1.wheel.translation.set(width*0.25,height*0.875);
-        wheel = wheel1;
-        wheel3.wheel.translation.set(width*0.5,height*0.65);
-        wheel3.wheel.rotation = degToRad(-60);
-        wheel2.wheel.rotation = degToRad(-120);
-        let fader1= makeFader(two,width*0.5,height*0.585,width/2.4);
-        let fader2= makeFader(two,width*0.4,height*0.86,width/2.4);
-        let fader3= makeFader(two,width*0.6,height*0.86,width/2.4);
+        let wheel1 = makeHexWheel(two,0,0,width/10,width*0.25,height*0.875,0,width/2.3,6);
+        let wheel2 = makeHexWheel(two,0,0,width/10,width*0.75,height*0.875,-120,width/2.3,3);
+        let wheel3 = makeHexWheel(two,0,0,width/10,width*0.5,height*0.65,-60,width/2.3,10);
+
+        wheels = {
+            one: wheel1,
+            two: wheel2,
+            three: wheel3
+        }
 
         setTimeout(() => {
-            wheel1.circle._renderer.elem.ontouchstart = ()=>console.log('hi');
-            wheel1.circle._renderer.elem.ontouchmove = (e)=>handleMove(two,e);
+            addListeners('one',180);
+            addListeners('two',-60);
+            addListeners('three',-120);
+        },200)
+
+
+        const addListeners=(wheelNum,angleComp)=>{
+            let circle = wheels[wheelNum].circle._renderer.elem;
             
-
-            console.log(wheel1.circle._renderer.elem);
-            
-        }, 1000);
-
-
-
-        fader1.fill = 'transparent';
-        fader2.fill = 'transparent';
-        fader3.fill = 'transparent';
-        // console.log(wheel1.circle.dashes);
-        fader1.dashes[0] = 5;
-        fader2.dashes[0] = 10;
-        fader3.dashes[0] = 6;
-        fader2.rotation = degToRad(60);
-        fader3.rotation = degToRad(-60);
-        // curve.translation.set(width*0.5,height*0.65)
+            let range =  wheelRange[wheelNum];
+            circle.ontouchstart = (e)=>handleMove(e,wheelNum,angleComp,range);
+            circle.ontouchmove = (e)=>handleMove(e,wheelNum,angleComp,range);
+            circle.ontouchend = (e)=>handleMove(e,wheelNum,angleComp,range);
+            let curve = wheels[wheelNum].curve._renderer.elem;
+            curve.ontouchstart = (e)=>handleMove(e,wheelNum,angleComp,range);
+            curve.ontouchmove = (e)=>handleMove(e,wheelNum,angleComp,range);
+            curve.ontouchend = (e)=>handleMove(e,wheelNum,angleComp,range);
+        }
         
+
+
         two.update();
     }
 
-    const makeFader =(two,x1,y1,length)=>{
-        
-        let curve = two.makeCurve(
-            x1 - length/2,y1,
-            x1 - length/2.5,y1-length/8,
-            x1 - length/3,y1-length/5.75,
-            x1 - length/4,y1-length/4.5,
-            x1,y1 - length/3.6,
-            x1 + length/4,y1-length/4.5,
-            x1 + length/3,y1-length/5.75,
-            x1 + length/2.5,y1-length/8,
-            x1 + length/2,y1,
-            true);
-        curve.linewidth = 1;
-        curve.opacity = 0.5;
-        curve.cap = 'rounded'
-        curve.subdivide();
-        // curve.dashes.offset = 4;
-        // curve.dashes.length = 2;
-        // console.log(curve);
-        
-        return curve;
-    }
+    
 
-    const makeHexWheel =(two,x,y,r)=>{
+    const makeHexWheel =(two,x,y,r,gX,gY,rot,length,dashes)=>{
         let circle = two.makeCircle(x,y-r*2 + 2,r/2);
 
-        let circle1 = two.makeCircle(x,y-r*2 + 2,r/5);
-        circle.stroke = 'transparent';
+        let curve = two.makeCurve(
+            x - length/2,y,
+            x - length/2.5,y-length/8,
+            x - length/3,y-length/5.75,
+            x - length/4,y-length/4.5,
+            x,y - length/3.6,
+            x + length/4,y-length/4.5,
+            x + length/3,y-length/5.75,
+            x + length/2.5,y-length/8,
+            x + length/2,y,
+            true);
+        curve.linewidth = 1;
+        curve.cap = 'rounded'
+        curve.dashes[0] = dashes;
+        curve.fill = 'transparent';
+        curve.translation.set(x+50,-r*0.8);
+        curve.rotation = degToRad(60);
+        let fader =  curve.clone();
+        fader.linewidth = 3;
+        curve.opacity = 0.5;
+        fader.ending = 0;
         circle.fill = 'transparent';
-
-        circle1.fill = 'black';
         
+  
       
         let hex1 = two.makePolygon(x+r/1.135 -1,r/2+y,r,6);
         hex1.rotation = 1.5708;
@@ -117,13 +154,19 @@ export const PlayView=props=>{
         hex2.rotation = 1.5708;
         let hex3 = two.makePolygon(x,-r,r,6);
         hex3.rotation = 1.5708;
-        let wheel = two.makeGroup(hex1,hex2,hex3,circle1,circle);        
-        return {wheel,hex1,hex2,hex3,circle};
+        let wheel1 = two.makeGroup(hex1,hex2,hex3,circle)
+        let wheel = two.makeGroup(wheel1,fader,curve);        
+        wheel.translation.set(gX,gY);
+        wheel.rotation = degToRad(rot);
+
+        return {wheel:wheel1,hex1,hex2,hex3,circle,curve,fader};
     }
 
 
     useEffect(()=>{
 
+
+        window.addEventListener( "touchstart", function(e){ onStart(e); }, false );
         updateSize();
         window.addEventListener('resize',(e)=>{updateSize()});
         return(()=>{
---
+++ src/utils/utils.js
@@ -0,0 +1,14 @@
+
+
+
+
+export const degToRad=(deg)=>{
+    let pi = Math.PI;
+    return deg * pi / 180;
+  }
+  
+export const radToDeg=(rad)=>{
+    let pi = Math.PI;
+    return rad * 180 / pi;
+}
+  
